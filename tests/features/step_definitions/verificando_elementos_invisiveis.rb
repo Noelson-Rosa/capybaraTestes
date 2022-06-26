f9547163207da34596ef9fbb2038ef6573307a9b
Noelson Rosa <41403798+Noelson-Rosa@users.noreply.github.com>
2022-06-26 12:53:33 -0300

    update de verificando elementos invisiveisII

--- tests/features/step_definitions/verificando_elementos_invisiveis.rb
+++ tests/features/step_definitions/verificando_elementos_invisiveis.rb
@@ -4,13 +4,14 @@ Quando('clico em um botao') do
 end
   
 Entao('verifico se o texto desaparece na tela com sucesso') do
-  page.assert_text(text, 'Você Clicou no Botão!')
-  page.has_text?('Você Clicou no Botão!')
-  have_text('Você Clicou no Botão!')
-
+  # page.assert_text(text, 'Você Clicou no Botão!')
+  # page.has_text?('Você Clicou no Botão!')
+  # have_text('Você Clicou no Botão!')
+  @texto = find('#div1')
+  expect(@texto.text). to eq 'Você Clicou no Botão!'
   find('#teste').click
    sleep(4)
-  assert_no_text(text, 'Você Clicou no Botão!')
-  has_no_text?('Você Clicou no Botão!')
+  # assert_no_text(text, 'Você Clicou no Botão!')
+  # has_no_text?('Você Clicou no Botão!')
    
 end
