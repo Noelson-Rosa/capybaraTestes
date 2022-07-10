b40bd5d66c93c46842e32b1da95e37f58a9d84a8
Noelson Rosa <41403798+Noelson-Rosa@users.noreply.github.com>
2022-07-10 18:00:37 -0300

    trabalhando com drag and drop arrastar para

---
+++ tests/features/specs/drag_and_drop.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+@dragdrop
+Funcionalidade: Usando drag e drop
+
+Cenario: Usar drag e drop
+Dado que eu estou na tela de dragdrop
+Quando movo o dragdrop 
---
+++ tests/features/step_definitions/drag_and_drop.rb
@@ -0,0 +1,12 @@
+Dado('que eu estou na tela de dragdrop') do
+   visit '/iteracoes/draganddrop' 
+end
+  
+Quando('movo o dragdrop') do
+  @primeiro_elemento = find('#winston')
+  @segundo_elemento = find('#dropzone')
+
+  @primeiro_elemento.drag_to(@segundo_elemento)   #drag_to  = arrastar_para
+  sleep(5)
+end
+  
